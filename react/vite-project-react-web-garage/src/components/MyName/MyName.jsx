56391bba7a41abea66169b4e93bb1ccff00cd46e
zaytsev <zaicev@mniti.ru>
2025-05-30 14:45:29 +0300

    practice react 1.5

--- react/vite-project-react-web-garage/src/components/MyName/MyName.jsx
+++ react/vite-project-react-web-garage/src/components/MyName/MyName.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './MyName.css'
+import {EmailCheck} from "../EmailCheck/EmailCheck.jsx";
 
 
 export const MyName = ({name}) => {
+
+    const cats = ['cat', 'cat1', 'cat2', 'cat3'];
+    const [condition, setCondition] = React.useState(false);
+
     return (
-        <div>
-           <h2>My name is {name}</h2>
-        </div>
+        <>
+            <div>
+                <button type={"button"} onClick={() => setCondition(!condition)}>Condition</button>
+                <h2>My name is {name}</h2>
+                {condition && <EmailCheck/>}
+                <ul>
+                    {cats.map((cat, index) => (
+                        <li key={index}>
+                            {cat}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
     );
 };
